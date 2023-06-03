ff8ea6b502bfd48e8edf324bb941223697b7b87e
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2023-06-03 16:13:50 +0500

    feat(start-screen): copy/paste whole file code

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,39 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class StartScreenWidget extends StatelessWidget {
-  const StartScreenWidget(this.startQuiz, {super.key});
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
 
   final void Function() startQuiz;
 
   @override
-  Widget build(BuildContext context) {
-    final ButtonStyle style = OutlinedButton.styleFrom(
-      foregroundColor: Colors.white,
-    );
-
+  Widget build(context) {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Opacity(
-            opacity: 0.5,
-            child: Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
-              color: const Color.fromARGB(150, 255, 255, 255),
-            ),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
-          // Opacity( //not recommended using this extra widget for opacity for performance optimization
-          //   opacity: 0.5,
+          // Opacity(
+          //   opacity: 0.6,
           //   child: Image.asset(
           //     'assets/images/quiz-logo.png',
           //     width: 300,
           //   ),
           // ),
-          const SizedBox(
-            height: 80,
-          ),
+          const SizedBox(height: 80),
           Text(
             'Learn Flutter the fun way!',
             style: GoogleFonts.lato(
@@ -43,11 +34,13 @@ class StartScreenWidget extends StatelessWidget {
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            style: style,
             onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text('Start Quiz'),
-          ),
+          )
         ],
       ),
     );
